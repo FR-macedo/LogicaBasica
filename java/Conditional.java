b91b36188d82f3cec4f11126e8aef47931c565ba
Francisco Macedo <macedo270701@gmail.com>
2024-08-27 09:46:46 -0300

    adição de switch case no conditional.java

--- java/Conditional.java
+++ java/Conditional.java
@@ -5,6 +5,7 @@ public class Conditional {
         int a = 10;
         int b = 20;
 
+        // If-else demonstration
         if (a > b) {
             System.out.println("A is greater than B");
         } else if (a < b) {
@@ -13,7 +14,42 @@ public class Conditional {
             System.out.println("A is equal to B");
         }
 
+        // Ternary operator demonstration
         String result = (a > b) ? "A is greater" : "A is not greater";
         System.out.println(result);
+
+        // Switch case demonstration
+        int day = 3;
+        String dayName;
+
+        switch (day) {
+            case 1:
+                dayName = "Sunday";
+                break;
+            case 2:
+                dayName = "Monday";
+                break;
+            case 3:
+                dayName = "Tuesday";
+                break;
+            case 4:
+                dayName = "Wednesday";
+                break;
+            case 5:
+                dayName = "Thursday";
+                break;
+            case 6:
+                dayName = "Friday";
+                break;
+            case 7:
+                dayName = "Saturday";
+                break;
+            default:
+                dayName = "Invalid day";
+                break;
+        }
+
+        System.out.println("Day " + day + " is " + dayName);
     }
 }
+
