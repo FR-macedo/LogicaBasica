ca6c2ae97c9164aaf48d5af87f58c1dbafa3e56f
Francisco Macedo <macedo270701@gmail.com>
2024-08-27 09:44:50 -0300

    criação do objects.java

--- java/Objects.java
+++ java/Objects.java
@@ -1,5 +1,26 @@
 package java;
 
 public class Objects {
-    
+    public static void main(String[] args) {
+        Person person = new Person("John", 25);
+        System.out.println(person.getName() + " is " + person.getAge() + " years old.");
+    }
+}
+
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
