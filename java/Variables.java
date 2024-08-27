d88a831b396a32b0d5954d98f5fb4bcf432e778f
Francisco Macedo <macedo270701@gmail.com>
2024-08-27 09:40:59 -0300

    criação do variables.java

--- java/Variables.java
+++ java/Variables.java
@@ -1,6 +1,38 @@
 package java;
 
 public class Variables {
+    public static void main(String[] args) {
+        int integerVar = 10;
+        double doubleVar = 20.5;
+        char charVar = 'A';
+        String stringVar = "Hello, World!";
+        boolean booleanVar = true;
 
-    
+        int[] arrayVar = {1, 2, 3, 4, 5};
+        int[][] matrixVar = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("String: " + stringVar);
+        System.out.println("Boolean: " + booleanVar);
+
+        System.out.print("Array: ");
+        for (int i : arrayVar) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println("Matrix: ");
+        for (int i = 0; i < matrixVar.length; i++) {
+            for (int j = 0; j < matrixVar[i].length; j++) {
+                System.out.print(matrixVar[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
