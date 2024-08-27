8ee9ac82729bf6e4d6eea64b96d62275e90f5542
Francisco Macedo <macedo270701@gmail.com>
2024-08-27 10:11:57 -0300

    adição do functions.js

---
+++ js/functions.js
@@ -0,0 +1,15 @@
+// Regular function declaration
+function add(a, b) {
+    return a + b;
+}
+
+// Arrow function declaration
+const printMessage = (message) => {
+    console.log(message);
+}
+
+// Calling the functions
+let result = add(10, 20);
+console.log("Sum:", result);
+
+printMessage("Hello, World!");
