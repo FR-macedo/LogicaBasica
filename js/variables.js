44bf4a9955df0f2184b7723b4d19364ce1a56333
Francisco Macedo <macedo270701@gmail.com>
2024-08-27 09:49:50 -0300

    edição do variables.js

--- js/variables.js
+++ js/variables.js
@@ -1,16 +1,28 @@
-const int = 2;
-console.log(int);
-let number = 1;
-console.log(number);
-let String = "Rede cidadã";
-console.log(String);
-let double = 3.141516;
-console.log(double);
-let boolean = true;
-console.log(boolean);
-let array = [1,2,3,4,5,6];
-console.log(array);
-let mixedarray = [1, 3.141516, "String para demonstração", false];
-console.log(mixedarray);
-let matrices = [[1,2,3],[1,2,3],[1,2,3]];
-console.log(matrices);
+// Demonstrating variable declaration with different data types
+let integerVar = 10;
+let doubleVar = 20.5;
+let stringVar = "Hello, World!";
+let booleanVar = true;
+
+// Demonstrating that variables in JavaScript can hold any type of data
+integerVar = "Now I'm a string!";
+doubleVar = [1, 2, 3, 4];
+booleanVar = { key: "value" };
+
+console.log("Integer (originally):", 10);
+console.log("Integer (changed to string):", integerVar);
+console.log("Double:", doubleVar);
+console.log("String:", stringVar);
+console.log("Boolean (changed to object):", booleanVar);
+
+// Demonstrating arrays and matrices
+let arrayVar = [1, 2, 3, 4, 5];
+let matrixVar = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+console.log("Array:", arrayVar);
+console.log("Matrix:");
+matrixVar.forEach(row => console.log(row));
