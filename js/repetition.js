eae61c525e19e21668c17b1e89aac4bfc99e560d
Francisco Macedo <macedo270701@gmail.com>
2024-08-27 09:50:55 -0300

    adição do repetition.js

---
+++ js/repetition.js
@@ -0,0 +1,21 @@
+// For loop demonstration
+console.log("For loop:");
+for (let i = 0; i < 5; i++) {
+    console.log(`i: ${i}`);
+}
+
+// While loop demonstration
+console.log("\nWhile loop:");
+let j = 0;
+while (j < 5) {
+    console.log(`j: ${j}`);
+    j++;
+}
+
+// Do-while loop demonstration
+console.log("\nDo-while loop:");
+let k = 0;
+do {
+    console.log(`k: ${k}`);
+    k++;
+} while (k < 5);
