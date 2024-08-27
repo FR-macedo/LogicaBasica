4549736ef7b0818bd11ca11a84dc22b95492e6da
Francisco Macedo <macedo270701@gmail.com>
2024-08-27 09:43:53 -0300

    criação do functions.java

--- java/Functions.java
+++ java/Functions.java
@@ -1,5 +1,19 @@
 package java;
 
 public class Functions {
-    
+    public static void main(String[] args) {
+        int result = add(10, 20);
+        System.out.println("Sum: " + result);
+
+        printMessage("Hello, World!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
 }
+
